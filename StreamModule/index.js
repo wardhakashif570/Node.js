3a6b1379b7786185b43b6af05253e1ccf91258c4
wardhakashif <wardhakashif570@gmail.com>
2020-11-06 11:20:07 -0800

    Stream Module

---
+++ StreamModule/index.js
@@ -0,0 +1,33 @@
+const fs=require('fs');
+const http=require('http');
+
+const server=http.createServer();
+
+
+server.on('request',(req,res)=>{
+ 
+// fs.readFile('input.txt',function(err,data){
+//     if(err)
+//     return console.error(err)
+//     res.end(data.toString());
+// });
+
+const rstream=fs.createReadStream('input.txt');
+rstream.pipe(res);
+
+// rstream.on('data',(chunkdata)=>{
+//     res.write(chunkdata);
+   
+// });
+// rstream.on('end',()=>{
+//     res.end();
+// });
+
+// rstream.on('error',(err)=>{
+//     console.log(err);
+//     res.end("file not found");
+// })
+
+});
+
+server.listen(8000,"127.0.0.1")
